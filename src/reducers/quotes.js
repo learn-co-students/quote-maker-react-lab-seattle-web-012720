db189e1d048ae1adf96c12980ed3758accb1422f
dianakw8591 <dianakw@gmail.com>
2020-04-22 19:10:13 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,21 @@
-export default (state = [], action) => {
+import uuid from 'uuid';
+
+// const id = uuid();
+
+export default function quotes(state = [], action) {
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      const id = uuid();
+      return [...state, {...action.quote, id, votes: 0}]
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case 'UPVOTE_QUOTE':
+      return state.map(quote => quote.id !== action.quoteId ? quote : {...quote, votes: quote.votes + 1})
+
+      case 'DOWNVOTE_QUOTE':
+      return state.map(quote => quote.id !== action.quoteId ? quote : quote.votes === 0 ? quote : {...quote, votes: quote.votes - 1})
+  }
   return state;
 }
